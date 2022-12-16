c1fd97379b5db3ba6490852ace175cd9c40ee58f
juliatag <julieta.jabbot@gmail.com>
2022-12-15 19:22:19 -0500

    setting up tailwind

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components";
+import "./App.scss";
 
 function App() {
   return (
+    //DO NOT include social icons,side dots or copyright footer on these pages
+    //we will do that after, on the global scale
     <div className="app">
+      {/* TODO: assigned to Zac */}
       <Navbar />
+      {/* TODO: assigned to Aftab */}
       <Header />
+      {/* TODO: assigned to Patrick */}
       <About />
+      {/* TODO: assigned to Julieta */}
       <Work />
+      {/* TODO: assigned to Aftab */}
       <Skills />
+      {/* TODO: assigned to Patrick */}
       <Testimonial />
+      {/* TODO: assigned to Julieta */}
       <Footer />
     </div>
   );
---
+++ frontend_react/tailwind.config.cjs
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  mode: "jit", // just in time (loads ?? only hwn needed)
+  theme: {
+    extend: {
+      colors: {
+        primary: "#edf2f8",
+        secondary: "#313bac",
+        black: "#030303",
+        lightGray: "#e4e4e4",
+        gray: "#6b7688",
+        brown: "#46364a",
+        white: "#ffffff",
+      },
+      fontFamily: {
+        dmSans: ["DM Sans", "sans-serif"],
+      },
+      spacing: {
+        "8xl": "96rem",
+        "9xl": "128rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+      xxl: "2000px",
+    },
+  },
+};
