873b5d370110bc080e8a6208fd3cb32ea93dca56
juliatag <julieta.jabbot@gmail.com>
2022-12-16 02:41:51 -0500

    .env fix by Aftab

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -1,14 +1,13 @@
+// require("dotenv").config();
 import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
-
+console.log();
 export const client = sanityClient({
-  // projectId: process.env.SANITY_STUDIO_API_PROJECT_ID,
-  projectId: "qqjhavhg",
+  projectId: import.meta.env.VITE_SANITY_STUDIO_API_PROJECT_ID,
   dataset: "production",
   apiVersion: "2022-02-01",
   useCdn: true,
-  // token: process.env.REACT_APP_SANITY_PROJECT_TOKEN,
-  token: "skerKaAFUYwPUQVbrQAVwZ8ZSwLnpMaZhrL93ZWemFfotpifwcJm0CDL1CtqpN5cYKFcdzL4px273UBWCW9bfRoOJiWenx2SUFFbL3d4RSFQFCs9ZJzspRoUWQeBfCYdiDE9YBwbccGpQnRNWaLoLX3XiJih1u1UQUShxZR8JrcLCOjT5NQd",
+  token: import.meta.env.VITE_REACT_APP_SANITY_PROJECT_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
