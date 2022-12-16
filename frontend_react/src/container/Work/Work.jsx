153f9cd5faea0445f2f09f05276d290ee636ab1f
juliatag <julieta.jabbot@gmail.com>
2022-12-16 00:33:03 -0500

    Added client, wraper and children components

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
 import "./Work.scss";
+import { AppWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
 
 const Work = () => {
   return <div>Work</div>;
