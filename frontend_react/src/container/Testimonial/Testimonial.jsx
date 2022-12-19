cf8e79dc7109dd438b0924bed35989886bab63c7
aftab87 <safdar_aftab@hotmail.com>
2022-12-19 15:37:13 -0500

    Converted testimonial to Tailwind

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 
 import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
-// import './Testimonial.scss';
+import './Testimonial.scss';
 
 const Testimonial = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -28,9 +28,35 @@ const Testimonial = () => {
     });
   }, []);
 
-  if (!testimonials.length) return null;
+  return (
+    <>
+      {testimonials.length && (
+        <>
+          <div className="app__testimonial-item app__flex">
+            <img src={urlFor(testimonials[currentIndex].imageurl)} alt={testimonials[currentIndex].name} />
+            <div className="app__testimonial-content">
+              <p className="p-text">{testimonials[currentIndex].feedback}</p>
+              <div>
+                <h4 className="bold-text"><span className='text-secondary'>{testimonials[currentIndex].name}</span></h4>
+                <h5 className="p-text"><span className='text-gray'>{testimonials[currentIndex].company}</span></h5>
+              </div>
+            </div>
+          </div>
 
-  /* <div className="app__testimonial-brands app__flex">
+          {/* flex justify-center mt-4 app__testimonial-btns app__flex */}
+          <div className="app__testimonial-btns app__flex">
+            <div className="app__flex" onClick={() => handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1)}>
+              <HiChevronLeft />
+            </div>
+
+            <div className="app__flex" onClick={() => handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1)}>
+              <HiChevronRight />
+            </div>
+          </div>
+        </>
+      )}
+
+      {/* <div className="app__testimonial-brands app__flex">
         {brands.map((brand) => (
           <motion.div
             whileInView={{ opacity: [0, 1] }}
@@ -40,38 +66,13 @@ const Testimonial = () => {
             <img src={urlFor(brand.ImgURL)} alt={brand.name} />
           </motion.div>
         ))}
-      </div> */
-
-  return (
-    <>
-      <div className='flex flex-col w-full mt-20'>
-        <div className="w-3/4 min-h-[210px] flex p-[2rem] rounded-[15px] ease-in-out app__flex bg-white mx-auto md:w-[450px] justify-center place-items-center flex-col sm:flex-row shadow-[0_0_30px_rgba(0,0,0,0.1)]">
-          <img className='w-[100px] h-[100px] rounded-[15px] object-cover' src={urlFor(testimonials[currentIndex].imageurl)} alt={testimonials[currentIndex].name} />
-          <div className="flex flex-col justify-around item-start flex-1 h-full px-8 text-left">
-            <p className="text-lg p-text">{testimonials[currentIndex].feedback}</p>
-            <div>
-              <h4 className="font-semibold mt-8 bold-text">{testimonials[currentIndex].name}</h4>
-              <h5 className="font-normal mt-[5px] p-text">{testimonials[currentIndex].company}</h5>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="flex justify-center mt-4 app__testimonial-btns app__flex">
-        <div className="flex justify-center items-center w-[50px] h-[50px] rounded-full m-4 transition-[background] duration-[0.3s] ease-in-out bg-white hover:bg-secondary" onClick={() => handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1)}>
-          <HiChevronLeft />
-        </div>
-
-        <div className="flex justify-center items-center w-[50px] h-[50px] rounded-full m-4 transition-[background] duration-[0.3s] ease-in-out bg-white hover:bg-secondary" onClick={() => handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1)}>
-          <HiChevronRight />
-        </div>
-      </div>
+      </div> */}
     </>
-  )
-};
+  );
+}
 
 export default AppWrap(
   MotionWrap(Testimonial, 'app__testimonial'),
   'testimonial',
-  'bg-primary',
+  'app__primarybg',
 );
