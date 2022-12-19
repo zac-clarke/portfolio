6e63f8db6774dbb399d32c31dc6ae3348d8005cb
juliatag <julieta.jabbot@gmail.com>
2022-12-19 13:01:35 -0500

    MotionWrappe.jsxr and  Footer.jsx

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -1,10 +1,78 @@
-import React from 'react'
-import './Footer.scss';
+import React, { useState } from "react";
+import { client } from "../../client";
+
+import { images } from "../../constants";
+import { AppWrap, MotionWrap } from "../../wrapper";
+import "./Footer.scss";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  const [formData, setFormData] = useState({ name: "", email: "", message: "" });
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const { name, email, message } = formData;
 
-export default Footer
+  const handleChandeInput = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = () => {
+    setLoading(true);
+
+    //following sanity guidelines
+    const contact = {
+      _type: "contact",
+      name: name,
+      emial: email,
+      message: message,
+    };
+
+    client.create(contact).then(() => {
+      setLoading(false);
+      setIsFormSubmitted(true);
+    });
+  };
+  return (
+    <>
+      <h2 className="head-text"> Get in toutch</h2>
+      <div className="app__footer-cards">
+        <div className="app__footer-card">
+          <img src={images.email} alt="email" />
+          <a href="mialto:example@mail.com" className="p-text">
+            example@mail.com
+          </a>
+        </div>
+        <div className="app__footer-card">
+          <img src={images.mobile} alt="email" />
+          <a href="tel: +1 (123) 456-7890" className="p-text">
+            +1 (123) 456-7890
+          </a>
+        </div>
+      </div>
+
+      {!isFormSubmitted ? (
+        <div className="app__footer-form app__flex">
+          <div className="app__flex">
+            <input className="p-text" type="text" placeholder="Your name.." name={name} value={name} onChange={handleChandeInput} />
+          </div>
+          <div className="app__flex">
+            <input className="p-text" type="email" placeholder="Your email.." name={email} value={email} onChange={handleChandeInput} />
+          </div>
+          <div>
+            <textarea className="p-text" placeholder="Your message.." name={message} value={message} onChange={handleChandeInput} />
+          </div>
+          <button type="button" className="p-text" onClick={handleSubmit}>
+            {loading ? "Sending..." : "Send Message"}
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h3 className="head-text">Thank You for reaching out</h3>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AppWrap(MotionWrap(Footer, "app__footer"), "footer", "bg-white");
--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { AiFillEye, AiFillGithub } from "react-icons/ai";
 import { animate, motion } from "framer-motion";
 import "./Work.scss";
-import { AppWrap } from "../../wrapper";
+import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 
 const Work = () => {
@@ -106,4 +106,4 @@ const Work = () => {
   );
 };
 
-export default AppWrap(Work, "work");
+export default AppWrap(MotionWrap(Work, "app__works"), "work", "bg-primary");
