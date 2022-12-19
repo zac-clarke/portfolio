03c6b9f56f478709c56461e2c536c2294604e1b6
aftab87 <safdar_aftab@hotmail.com>
2022-12-19 14:12:02 -0500

    Converted About section to Tailwind

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 import { AppWrap, MotionWrap } from '../../wrapper/';
-// import './About.scss';
+import './About.scss';
 // import './About.css';
 import { urlFor, client } from '../../client';
 // import {images} from '../../constants';
@@ -28,25 +28,23 @@ const About = () => {
 
   return (
     <>
-      <div className='bg-white'>
-        <h2 className="font-dmSans text-[44px] font-extrabold text-center leading-[3rem]">I Know that <span className='font-extrabold text-secondary'>Good Design</span> <br />means  <span className='font-extrabold text-secondary'>Good Business</span></h2>
+        <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
 
-        <div className="flex justify-center flex-wrap mt-[32px]">
+        <div className="app__profiles">
           {abouts.map((about, index) => (
             <motion.div
               whileInView={{ opacity: 1 }}
               whileHover={{ scale: 1.1 }}
               transition={{ duration: 0.5, type: 'tween' }}
-              className="w-[370px] md:w-[190px] flex flex-col gap-[2rem] m-[2rem]"
+              className="app__profile-item"
               key={about.title + index}
             >
               <img className="rounded-[15px]"  src={urlFor(about.imgUrl)} alt={about.title} />
-              <h2 className="mt-[20px] font-bold" >{about.title}</h2>
-              <p className="text-sm text-align text-gray mt-[10px]">{about.description}</p>
+              <h2 className="bold-text mt-[20px]" >{about.title}</h2>
+              <p className="p-text mt-[10px]">{about.description}</p>
             </motion.div>
           ))}
         </div>
-      </div>
     </>
   );
 };
