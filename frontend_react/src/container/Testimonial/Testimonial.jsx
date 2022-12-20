d61ea957eebc0b4494241a4c8e68e9b8f5ff7e6e
juliatag <julieta.jabbot@gmail.com>
2022-12-20 01:15:01 -0500

    layout and brands fix

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
-import { motion } from 'framer-motion';
+import React, { useState, useEffect } from "react";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
+import { motion } from "framer-motion";
 
-import { AppWrap, MotionWrap } from '../../wrapper';
-import { urlFor, client } from '../../client';
-import './Testimonial.scss';
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
+import "./Testimonial.scss";
 
 const Testimonial = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -24,6 +24,7 @@ const Testimonial = () => {
     });
 
     client.fetch(brandsQuery).then((data) => {
+      console.log(data);
       setBrands(data);
     });
   }, []);
@@ -37,42 +38,42 @@ const Testimonial = () => {
             <div className="app__testimonial-content">
               <p className="p-text">{testimonials[currentIndex].feedback}</p>
               <div>
-                <h4 className="bold-text"><span className='text-secondary'>{testimonials[currentIndex].name}</span></h4>
-                <h5 className="p-text"><span className='text-gray'>{testimonials[currentIndex].company}</span></h5>
+                <h4 className="bold-text">
+                  <span className="text-secondary">{testimonials[currentIndex].name}</span>
+                </h4>
+                <h5 className="p-text">
+                  <span className="text-gray">{testimonials[currentIndex].company}</span>
+                </h5>
               </div>
             </div>
           </div>
 
           {/* flex justify-center mt-4 app__testimonial-btns app__flex */}
           <div className="app__testimonial-btns app__flex">
-            <div className="app__flex" onClick={() => handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1)}>
-              <HiChevronLeft />
+            <div className="app__flex group" onClick={() => handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1)}>
+              <HiChevronLeft className="group-hover:text-white" />
             </div>
 
-            <div className="app__flex" onClick={() => handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1)}>
-              <HiChevronRight />
+            <div className="app__flex group" onClick={() => handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1)}>
+              <HiChevronRight className="group-hover:text-white" />
             </div>
           </div>
         </>
       )}
-
-      {/* <div className="app__testimonial-brands app__flex">
+      {/* prettier-ignore */}
+      <div className="app__testimonial-brands app__flex">
         {brands.map((brand) => (
-          <motion.div
-            whileInView={{ opacity: [0, 1] }}
-            transition={{ duration: 0.5, type: 'tween' }}
+          <motion.div 
+            whileInView={{ opacity: [0, 1] }} 
+            transition={{ duration: 0.5, type: "tween" }} 
             key={brand._id}
           >
-            <img src={urlFor(brand.ImgURL)} alt={brand.name} />
+            <img src={urlFor(brand.imgUrl)} alt={brand.name} />
           </motion.div>
         ))}
-      </div> */}
+      </div>
     </>
   );
-}
+};
 
-export default AppWrap(
-  MotionWrap(Testimonial, 'app__testimonial'),
-  'testimonial',
-  'app__primarybg',
-);
+export default AppWrap(MotionWrap(Testimonial, "app__testimonial"), "testimonial", "app__primarybg");
