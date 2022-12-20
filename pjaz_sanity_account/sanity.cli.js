fdb7a1c4d544a21bdad38e26923914fdaddea508
zac <zacclarke@gmail.com>
2022-12-20 14:02:30 -0500

    changed sanity

--- pjaz_sanity_account/sanity.cli.js
+++ pjaz_sanity_account/sanity.cli.js
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '6q5wk69m',
-    dataset: 'production',
-  },
+    dataset: 'production'
+  }
 })
